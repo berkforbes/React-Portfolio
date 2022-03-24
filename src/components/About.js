d78f099a4fe2db8e7d15ec7284caed659a622cd4
Berk Forbes <91864476+berkforbes@users.noreply.github.com>
2022-03-23 18:04:34 -0600

    change file structure, install jquery and react ga, prepped components for fxs

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,15 @@
 import React from 'react'
-import coverImage from "../../assets/cover/cover-image.jpg";
+
 
 function About() {
 
   return (
     <section className="my-5" >
       <h1 id="about">Berk Forbes</h1>
-      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
-      <div className="my-2">
+
         <p>
       </p>
-      </div>
+
     </section>
   )
 }
---
+++ src/components/Portfolio.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Portfolio() {
+
+    return (
+      <section className="my-5" >
+      </section>
+    )
+  }
+  
+  export default Portfolio
