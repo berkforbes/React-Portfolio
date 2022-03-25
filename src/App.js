ca3fc810f76df80252e9e8d8e6dd349c462e0ba0
Berk Forbes <91864476+berkforbes@users.noreply.github.com>
2022-03-24 22:16:33 -0600

    debugging

--- src/App.js
+++ src/App.js
@@ -2,27 +2,55 @@ import React, { Component } from 'react';
 import ReactGA from 'react-ga';
 import $ from 'jquery';
 import './App.css';
-import Nav from './components/Nav';
-import Footer from './components/Footer';
-import About from './components/About';
-import Resume from './components/Resume';
-import Contact from './components/Contact';
-import Portfolio from './components/Portfolio';
-import personalData from './PersonalData'
+import Nav from './Components/Nav';
+import Footer from './Components/Footer';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import Contact from './Components/Contact';
+import Portfolio from './Components/Portfolio';
 
 class App extends Component {
 
-  
+  constructor(props){
+    super(props);
+    this.state = {
+      foo: 'bar',
+      resumeData: {}
+    };
+
+    ReactGA.initialize('UA-110570651-1');
+    ReactGA.pageview(window.location.pathname);
+
+  }
+
+  getResumeData(){
+    $.ajax({
+      url:'./resumeData.json',
+      dataType:'json',
+      cache: false,
+      success: function(data){
+        this.setState({resumeData: data});
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log(err);
+        alert(err);
+      }
+    });
+  }
+
+  componentDidMount(){
+    this.getResumeData();
+  }
 
   render() {
     return (
       <div className="App">
-        <Nav personalData={personalData}/>
-        <About personalData={personalData}/>
-        <Resume personalData={personalData}/>
-        <Portfolio personalData={personalData}/>
-        <Contact personalData={personalData}/>
-        <Footer personalData={personalData}/>
+        <Nav data={this.state.resumeData.main}/>
+        <About data={this.state.resumeData.main}/>
+        <Resume data={this.state.resumeData.resume}/>
+        <Portfolio data={this.state.resumeData.portfolio}/>
+        <Contact data={this.state.resumeData.main} repos={this.state.resumeData.portfolio}/>
+        <Footer data={this.state.resumeData.main}/>
       </div>
     );
   }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 
-
-export default class Nav extends Component {
+class Nav extends Component {
   render() {
 
-    let personalData = this.props.personalData;
+    if(this.props.data){
+       var project = this.props.data.project;
+       var instagram = this.props.data.instagram;
+       var github = this.props.data.github;
+      var name = this.props.data.name;
+      var description= this.props.data.description;
+      var city= this.props.data.address.city;
+      var networks= this.props.data.social.map(function(network){
+        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+      })
+    }
 
     return (
-       <React.Fragment>
       <header id="home">
       <nav id="nav-wrap">
          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
@@ -18,20 +26,20 @@ export default class Nav extends Component {
             <li><a className="smoothscroll" href="#about">About</a></li>
 	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
             <li><a className="smoothscroll" href="#portfolio">Projects</a></li>
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
+            <li><a className="smoothscroll" href="#contact">Contact Me</a></li>
          </ul>
       </nav>
 
       <div className="row banner">
       
          <div className="banner-text">
-            <h1 className="responsive-headline">{personalData.name}</h1>
-            <h3>{personalData.description}.</h3>
+            <h1 className="responsive-headline">{name}</h1>
+            <h3>{description}.</h3>
             <hr />
             <ul className="social">
-               <a href={personalData.project} target="_blank" className="button btn github-btn"><i className="fa fa-linkedin"></i>LinkedIn</a>
-               <a href={personalData.github} target="_blank" className="button btn github-btn"><i className="fa fa-github"></i>Github</a>
-               <a href={personalData.instagram} target="_blank" className="button btn github-btn"><i className="fa fa-instagram"></i>Instagram</a>
+               <a href={project} target="_blank" className="button btn github-btn"><i className="fa fa-linkedin"></i>LinkedIn</a>
+               <a href={github} target="_blank" className="button btn github-btn"><i className="fa fa-github"></i>Github</a>
+               <a href={instagram} target="_blank" className="button btn github-btn"><i className="fa fa-instagram"></i>Instagram</a>
             </ul>
          </div>
       </div>
@@ -41,7 +49,8 @@ export default class Nav extends Component {
       </p>
 
    </header>
-   </React.Fragment>
     );
   }
 }
+
+export default Nav;
